c8a53613a60a2522f72f6109f9962c4efcc0991f
JingYu Huang <huangjingyu@live.com>
2012-08-29 10:09:07 +0800

    add geoservice

---
+++ yaogan-service-impl/src/main/java/com/rockontrol/yaogan/service/GeoServiceImpl.java
@@ -0,0 +1,12 @@
+package com.rockontrol.yaogan.service;
+
+import java.io.File;
+
+public class GeoServiceImpl implements GeoService {
+
+   @Override
+   public void uploadShapeFile(String mineName, String year, File shapeFile) {
+      // TODO Auto-generated method stub
+   }
+
+}
