63a0aabdf9c9b335c8b2b2b457e131eba5ea82fb
JingYu Huang <huangjingyu@live.com>
2012-09-03 14:27:33 +0800

    add dojo

--- yaogan-service-impl/src/main/java/com/rockontrol/yaogan/service/YaoganServiceImpl.java
+++ yaogan-service-impl/src/main/java/com/rockontrol/yaogan/service/YaoganServiceImpl.java
@@ -1,5 +1,46 @@
 package com.rockontrol.yaogan.service;
 
+import java.util.List;
+
+import com.rockontrol.yaogan.model.Place;
+import com.rockontrol.yaogan.model.User;
+
 public class YaoganServiceImpl implements IYaoganService {
+
+   @Override
+   public List<Place> getAllPlaces(User caller) {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public List<Place> getPlacesVisibleToUser(User caller, Long userId) {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public List<String> getAvailableTimeOptions(User caller, Long placeId) {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public List<User> getAllUsersOfOrg(User caller, Long orgId) {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public void sharePlacesToUser(User caller, Long userId, Long[] placeId) {
+      // TODO Auto-generated method stub
+
+   }
+
+   @Override
+   public void unsharePlaceToUser(User caller, Long userId, Long placeId) {
+      // TODO Auto-generated method stub
+
+   }
 
 }
--- yaogan-service/src/main/java/com/rockontrol/yaogan/service/IYaoganService.java
+++ yaogan-service/src/main/java/com/rockontrol/yaogan/service/IYaoganService.java
@@ -1,5 +1,21 @@
 package com.rockontrol.yaogan.service;
 
+import java.util.List;
+
+import com.rockontrol.yaogan.model.Place;
+import com.rockontrol.yaogan.model.User;
+
 public interface IYaoganService {
 
+   public List<Place> getAllPlaces(User caller);
+
+   public List<Place> getPlacesVisibleToUser(User caller, Long userId);
+
+   public List<String> getAvailableTimeOptions(User caller, Long placeId);
+
+   public List<User> getAllUsersOfOrg(User caller, Long orgId);
+
+   public void sharePlacesToUser(User caller, Long userId, Long[] placeId);
+
+   public void unsharePlaceToUser(User caller, Long userId, Long placeId);
 }
