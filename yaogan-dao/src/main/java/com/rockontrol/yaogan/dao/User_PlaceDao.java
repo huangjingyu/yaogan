23abca18ded2c648bea754d85f6bfb231fc24273
JingYu Huang <huangjingyu@live.com>
2012-09-04 12:38:51 +0800

    rename

--- yaogan-dao/src/main/java/com/rockontrol/yaogan/dao/User_PlaceDao.java
+++
@@ -1,8 +0,0 @@
-package com.rockontrol.yaogan.dao;
-
-import com.rockontrol.yaogan.model.UserPlace;
-
-public interface User_PlaceDao {
-
-    void insertUser_place(UserPlace userPlace);
-}
--- yaogan-service/src/main/java/com/rockontrol/yaogan/service/User_PlaceService.java
+++
@@ -1,8 +0,0 @@
-package com.rockontrol.yaogan.service;
-
-import com.rockontrol.yaogan.model.UserPlace;
-
-public interface User_PlaceService {
-    public void insertUser_place(UserPlace userPlace);
-
-}
