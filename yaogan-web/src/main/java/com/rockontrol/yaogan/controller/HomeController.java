4c0e8caab50987b3c5adfb093766ee2fd159fdf1
JingYu Huang <huangjingyu@live.com>
2012-09-12 12:31:02 +0800

    add auth control, add log4j

--- yaogan-web/src/main/java/com/rockontrol/yaogan/controller/HomeController.java
+++ yaogan-web/src/main/java/com/rockontrol/yaogan/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.rockontrol.yaogan.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import com.rockontrol.yaogan.util.GlobalConfig;
 
 @Controller
 public class HomeController {
+   private static final Logger log = Logger.getLogger(HomeController.class);
 
    @Autowired
    private ISecurityManager _secMng;
@@ -17,9 +19,11 @@ public class HomeController {
    @RequestMapping("/")
    public String home() {
       User user = _secMng.currentUser();
-      if (user != null && user.getIsAdmin()) {
+      if (user != null && user.getIsAdmin().booleanValue()) {
+         log.info("user is admin");
          return "redirect:" + GlobalConfig.getProperties().getString("homepage.admin");
       } else {
+         log.info("user is not admin");
          return "redirect:" + GlobalConfig.getProperties().getString("homepage.user");
       }
    }
