68098843587a68aab0d5e0a49df659d1078f8d7a
JingYu Huang <huangjingyu@live.com>
2012-09-12 13:18:49 +0800

    bug fix, when refresh page, the portal header would mess when using chrome

--- yaogan-web/src/main/java/com/rockontrol/yaogan/controller/HomeController.java
+++ yaogan-web/src/main/java/com/rockontrol/yaogan/controller/HomeController.java
@@ -1,6 +1,5 @@
 package com.rockontrol.yaogan.controller;
 
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +10,6 @@ import com.rockontrol.yaogan.util.GlobalConfig;
 
 @Controller
 public class HomeController {
-   private static final Logger log = Logger.getLogger(HomeController.class);
 
    @Autowired
    private ISecurityManager _secMng;
@@ -20,10 +18,8 @@ public class HomeController {
    public String home() {
       User user = _secMng.currentUser();
       if (user != null && user.getIsAdmin().booleanValue()) {
-         log.info("user is admin");
          return "redirect:" + GlobalConfig.getProperties().getString("homepage.admin");
       } else {
-         log.info("user is not admin");
          return "redirect:" + GlobalConfig.getProperties().getString("homepage.user");
       }
    }
